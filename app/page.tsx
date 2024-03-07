feb09591f9592c459606e62548d706de4b25c6ae
GERISON KIMATHI <gerkim62@gmail.com>
2024-03-07 11:22:06 +0300

    Disable "Enter Courses Manually" button and add a message about it coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -20,10 +20,14 @@ function LandingPage() {
           >
             Log in with iCampus
           </Link>
-          <button className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition duration-300 dark:bg-blue-400 dark:hover:bg-blue-500 text-left">
+          <button
+            disabled
+            className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition duration-300 dark:bg-blue-400 dark:hover:bg-blue-500 text-left"
+          >
             Enter Courses Manually
           </button>
         </div>
+        <p className="text-center text-red-400 my-4">Enter courses manually is coming soon</p>
         <p className="text-lg text-center">
           Did you encounter any issues?{" "}
           <Link
